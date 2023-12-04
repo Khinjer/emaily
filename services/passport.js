f9f7555b8733b5f771eeffa887778b16302dd292
Khinjer <khinjer@gmail.com>
2023-12-04 21:19:11 +0100

    fixed google strategy proxy issue

--- services/passport.js
+++ services/passport.js
@@ -18,6 +18,7 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: "/auth/google/callback",
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleID: profile.id }).then((existingUser) => {
